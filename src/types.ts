8f914f45b9d170a0b62740868478b95807577397
ChandrakantPal <sanjay161097@gmail.com>
2021-03-21 22:44:11 +0530

    user type contact type

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface User {
+  email: string
+  token: string
+  username: string
+  createdAt: string
+}
+
+export interface Contact {
+  id: string
+  createdAt: string
+  username: string
+  contactname: string
+  contactemail: string
+}
