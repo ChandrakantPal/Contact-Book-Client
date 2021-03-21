21417fc5c0b68e0ec392a80264ef384637b2f236
ChandrakantPal <sanjay161097@gmail.com>
2021-03-21 22:47:37 +0530

    input group

---
+++ src/components/InputGroup.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames'
+
+interface InputGroupProps {
+  type: string
+  placeholder: string
+  value: string
+  error?: string | undefined
+  setValue: (str: string) => void
+}
+
+const InputGroup: React.FC<InputGroupProps> = ({
+  type,
+  placeholder,
+  value,
+  error,
+  setValue,
+}) => {
+  return (
+    <div className="my-4">
+      <input
+        type={type}
+        className={classNames(
+          'w-full p-3 transition duration-200 border border-gray-300 outline-none rounded-xl bg-gray-50 focus:bg-white hover:bg-white',
+          { 'border-red-500': error }
+        )}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-600">{error}</small>
+    </div>
+  )
+}
+
+export default InputGroup
