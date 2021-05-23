b5d44939d50921f6cf427bba236b545f35befd6d
ChandrakantPal <sanjay161097@gmail.com>
2021-05-23 21:20:24 +0530

    useEffeact in auth contet auth_check dispatch

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,5 +1,5 @@
 import jwt_decode, { JwtPayload } from 'jwt-decode'
-import { createContext, useContext, useReducer } from 'react'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 import { User } from '../types'
 
 interface State {
@@ -65,18 +65,17 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     authenticated: false,
   })
 
-  // useEffect(() => {
-  //   // const decodeToken: any = jwtDecode(localStorage.getItem('token'))
-  //   // console.log(decodeToken)
+  useEffect(() => {
+    const decodeToken = jwt_decode<JwtPayload>(localStorage.getItem('token'))
+    // console.log(decodeToken)
 
-  //   // if (decodeToken.exp * 1000 < Date.now()) {
-  //   //   localStorage.removeItem('token')
-  //   //   dispatch('LOGOUT')
-  //   // } else
-  //   if (localStorage.getItem('token')) {
-  //     dispatch('AUTH_CHECK')
-  //   }
-  // }, [])
+    if (decodeToken.exp * 1000 < Date.now()) {
+      localStorage.removeItem('token')
+      dispatch('LOGOUT')
+    } else if (localStorage.getItem('token')) {
+      dispatch('AUTH_CHECK')
+    }
+  }, [])
 
   const dispatch = (type: string, payload?: any) =>
     defaultDispatch({ type, payload })
